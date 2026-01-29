aaf7986821f565ff1f2ec3cc7d072296ce627885
txack <txack.1@gmail.com>
2026-01-29 10:27:05 +0100

    run self-hosted runner

--- src/app.py
+++ src/app.py
@@ -14,7 +14,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
         'hostname': socket.gethostname(),
-        'message': 'You are doing well! :)'
+        'message': 'You are doing well :)'
     })
 
 @app.route('/api/v1/healthz')
